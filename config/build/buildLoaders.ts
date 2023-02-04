1194acb88c22bc34151d73d5c318fea278a61f36
DASenkiv <dasenkiv@gmail.com>
2023-02-04 16:22:50 +0300

    feat: add React, scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,19 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from js strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ]
+    }
+
+    // если работает без TS - подключить babel-loader
     const typescriptLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',
@@ -9,6 +22,7 @@ export function buildLoaders(): webpack.RuleSetRule[] {
         };
 
     return [
-        typescriptLoader
+        typescriptLoader,
+        cssLoader
     ]
 }
