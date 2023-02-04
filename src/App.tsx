0fb5d051953e888eda60c29b174100698e3a0f32
DASenkiv <dasenkiv@gmail.com>
2023-02-04 23:07:31 +0300

    feat: add routing, code splitting, lazy loading, Suspense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 
-import {Counter} from "./components/Counter";
 import './index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 export const App = () => {
     return (
         <div className={'app'}>
-            sfd
-          <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
