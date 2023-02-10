18c056ab1843599219b2a786848e32b1fa35dd2e
DASenkiv <dasenkiv@gmail.com>
2023-02-10 08:30:38 +0300

    feat: add Button component, svg, file loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,3 @@
-import {Link} from 'react-router-dom';
-
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {classNames} from "shared/lib/classNames/classNames";
@@ -9,12 +7,11 @@ import './styles/index.scss';
 
 
 export const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
             <AppRouter/>
-            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
