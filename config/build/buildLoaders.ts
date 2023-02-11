1fe7e8c67eee5a03e6de986c1c92e5a4b962880b
DASenkiv <dasenkiv@gmail.com>
2023-02-11 16:48:35 +0300

    feat: add babel plugin for extract translation keys

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,25 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -40,16 +59,17 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
-    const fileLoader =  {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
-        };
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
 
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
