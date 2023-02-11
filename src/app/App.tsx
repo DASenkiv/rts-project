ac353629b2fa4a957c94be2572e46fbc8c4bce3f
DASenkiv <dasenkiv@gmail.com>
2023-02-11 13:15:29 +0300

    feat: add Sidebar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {classNames} from "shared/lib/classNames/classNames";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 import './styles/index.scss';
 
@@ -11,7 +12,10 @@ export const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className={'content-page'}>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import {FC, useState} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import cls from './Sidebar.module.scss'
+
+interface SidebarProps {
+    className?: string
+}
+
+export const Sidebar: FC<SidebarProps> = (props) => {
+
+    const {className, children, ...otherProps} = props;
+    const [collapsed, setCollapsed] = useState(false);
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+
+    return (
+        <div
+            className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}
+            {...otherProps}
+        >
+        <button onClick={onToggle}>toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher/>
+            </div>
+        </div>
+    );
+};
