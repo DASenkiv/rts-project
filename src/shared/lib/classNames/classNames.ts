6b22dc78f8e501b2a536e274b925a7473e9387e3
DASenkiv <dasenkiv@gmail.com>
2023-02-12 11:02:11 +0300

    feat: add jest config, add first test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,13 @@
 type Mods = Record<string, boolean>
 
-export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+export function classNames(
+    cls: string,
+    mods: Mods = {},
+    additional: string[] = [],
+): string {
     return [
         cls,
-        Object.entries(mods)
+        ...Object.entries(mods)
             .filter(([, value]) => Boolean(value))
             .map(([cls]) => cls),
         ...additional.filter(Boolean),
